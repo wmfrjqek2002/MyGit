cf686346c45418c1449962d22e25d09a3462ae5b
taehoon <wmfrjqek01@naver.com>
2023-12-23 14:05:09 +0900

    add a println line

--- mygit/src/mygit/A.java
+++ mygit/src/mygit/A.java
@@ -2,5 +2,6 @@ package mygit;
 
 public class A {
 	public static void main(String[] args) {
-		System.out.println("Hello world");	}
+		System.out.println("Hello world");
+		System.out.println("this is 2nd version");}
 }
