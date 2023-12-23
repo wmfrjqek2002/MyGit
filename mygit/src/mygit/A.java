cda56b1cd1798bab0b760798bf564b4f74b0d553
taehoon <wmfrjqek01@naver.com>
2023-12-23 14:08:49 +0900

    dev b branch

--- mygit/src/mygit/A.java
+++ mygit/src/mygit/A.java
@@ -3,5 +3,8 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("this is 2nd version");}
+		System.out.println("this is 2nd version");
+		System.out.println("dev b branch");
+	}
+
 }
--- mygit/src/mygit/B.java
+++ mygit/src/mygit/B.java
@@ -1,5 +1,9 @@
 package mygit;
 
 public class B {
+	public static void main(String[] args) {
+		System.out.println("dev b branch");
+		
+	}
 
 }
